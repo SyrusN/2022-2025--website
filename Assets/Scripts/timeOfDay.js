ed411c1c3186b311dfe0a9512240630a8e46dc4e
Syrus Nelson <syrus.n.career@gmail.com>
2025-02-01 18:04:37 -0800

    Transferred files over to this new repository.

---
+++ Assets/Scripts/timeOfDay.js
@@ -0,0 +1,22 @@
+(function () {
+   const date = new Date();
+   const hour = date.getHours();
+   if (hour > 20) {
+      document.getElementById("greeting").innerHTML = "Good Night Fellow Web Traveler";
+   }
+   else if (hour >= 0 && hour <= 4) {
+      document.getElementById("greeting").innerHTML = "Good Night Fellow Web Traveler";
+   }
+   else if (hour > 17) {
+      document.getElementById("greeting").innerHTML = "Good Evening Fellow Web Traveler";
+   }
+   else if (hour > 11) {
+      document.getElementById("greeting").innerHTML = "Good Afternoon Fellow Web Traveler";
+   }
+   else if (hour < 11 && hour > 4) {
+      document.getElementById("greeting").innerHTML = "Good Morning Fellow Web Traveler";
+   } else {
+      console.log("Something Exploded...");
+   }
+   
+}) ()
